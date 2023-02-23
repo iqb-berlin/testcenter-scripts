91c94e9558e937b594c618b941301c17418c3283
paflov <philipp.franck@hu-berlin.de>
2023-02-23 14:19:37 +0100

    also logs

--- loading_test/audit_local.sh
+++ loading_test/audit_local.sh
@@ -15,6 +15,9 @@ if [[ $AUDIT_MODE == 'local' ]]; then
   watch -n"$REPORT_FREQUENCY" bash ./loading_test/mimic_docker_stats.sh &
 elif [[ $AUDIT_MODE == 'docker' ]]; then
   docker stats >> "$WORKING_DIR/stats.log" &
+  docker logs -f --tail 0 testcenter-backend >> "$WORKING_DIR/backend.log" &
+  docker logs -f --tail 0 testcenter-traefik >> "$WORKING_DIR/traefik.log" &
+  docker logs -f --tail 0 testcenter-db >> "$WORKING_DIR/db.log" &
 else
   echo "Unknown mode: $MODE"
   exit 1
@@ -23,8 +26,11 @@ fi
 # Run stuff
 eval "$ACTION" >> "$WORKING_DIR/output.log" 2>"$WORKING_DIR/error.log"
 
-# stop recorder
+# stop recorders
 kill %1
+kill %2
+kill %3
+kill %4
 
 # stop timer
 END_TS=$(date +"%s%N")
