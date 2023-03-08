0849e8cef0eb89b660b44379189dae2ffa5c37b4
Your Name <you@example.com>
2023-03-08 15:17:59 +0000

    cleanup

--- loading_test/run_parallel.sh
+++ loading_test/run_parallel.sh
@@ -2,9 +2,11 @@
 
 source ./loading_test/loading_test.env
 
-ulimit -u $ULIMIT
-ulimit -n $ULIMIT
+#ulimit -Su $ULIMIT
+#ulimit -Sn $ULIMIT
+ulimit -n `ulimit -Hn`
 
 SEQ=($(seq $RUNS))
 
-parallel --jobs "$RUNS_AT_ONCE" --results "$WORKING_DIR/{}.log" --line-buffer bash "$WORKING_DIR/script.sh" ::: "${SEQ[@]}"
+parallel -j0 --results "$WORKING_DIR/{}.log" --line-buffer bash "$WORKING_DIR/script.sh" ::: "${SEQ[@]}"
+
