af24d54c6449004da098490cf71cd8d5abb06342
Paflov <paf@titelfrei.de>
2023-03-08 17:04:55 +0100

    don't use jq

--- loading_test/create_test_script.php
+++ loading_test/create_test_script.php
@@ -65,7 +65,9 @@ curl --location --silent --show-error \
 --data-raw '{"name":"$E->TC_USER_NAME","password":"$E->TC_PASSWORD"}'
 )
 
-AUTH_TOKEN=$(jq -r '.token' <<< "\$LOGIN_RESULT")
+REGEX='"token":"([a-zA-Z0-9.]+)"'
+[[ \$LOGIN_RESULT =~ \$REGEX ]]
+AUTH_TOKEN=\${BASH_REMATCH[1]}
 
 
 # the FE always runs a get session call after
