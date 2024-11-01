4bedcc3db44920ce304d829be799e3b0e39091b1
paflov <philipp.franck@hu-berlin.de>
2024-11-01 10:11:41 +0100

    add make clean

--- makefile-plus.mk
+++ makefile-plus.mk
@@ -3,3 +3,10 @@
 run:
 	cd $(TC_BASE_DIR) &&\
 	docker compose --file docker/docker-compose.yml --file docker/docker-compose.dev.yml up $(service)
+
+clean:
+	make down && \
+	docker container stop $(docker ps -a -q) || true && \
+	docker container rm -f $(docker ps -a -q) || true && \
+	docker volume rm $(docker volume ls -q) || true && \
+  docker volume prune --all -f
